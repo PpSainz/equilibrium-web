c69cf2e3b94533dedda0d1e85185460bb0221c05
José Luis Becerra Sainz <88699294+PpSainz@users.noreply.github.com>
2025-05-06 22:12:47 -0600

    fix(footer)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,7 +11,7 @@ export const routes: Routes = [
     pathMatch: 'full'
   },
   {
-    path: '',
+    path: 'home',
     component: HomeComponent,
     title: 'Equilibrium - Mental Health Support for Students'
   },
