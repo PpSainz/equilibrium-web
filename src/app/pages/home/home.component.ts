0536c7099ed098ba4e67251b8a7e68ded3c4e566
José Luis Becerra Sainz <88699294+PpSainz@users.noreply.github.com>
2025-05-06 20:39:28 -0600

    homepage2

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import feather from 'feather-icons';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,17 @@ import { CommonModule } from '@angular/common';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
   // You can add component logic here if needed
   constructor() {}
+
+  ngAfterViewInit() {
+    console.log('Inicializando Feather Icons...');
+    try {
+      feather.replace();
+      console.log('Feather Icons inicializados correctamente');
+    } catch (error) {
+      console.error('Error al inicializar Feather Icons:', error);
+    }
+  }
 }
